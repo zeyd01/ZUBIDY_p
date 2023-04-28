99a917b230a00b8a0c3f695777f696c5e3a5d9a0
zeyd01 <zeydiyigoren@gmail.com>
2023-04-28 13:09:21 +0300

    registerSite1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import logo from './logo-01-01.png';
 import './App.css';
 
+function boxControl() {
+
+  const c1 = document.getElementById("c1");
+  const c2=document.getElementById("c2");
+  const t1 = document.getElementById("t1");
+  const t2=document.getElementById("t2");
+
+  if (c1.checked === true){
+
+      t1.style.display = "block";
+    } else {
+      t1.style.display = "none";
+    }
+  if (c2.checked && c1.checked === true){
+
+    t2.style.display = "block";
+  } else {
+    t2.style.display = "none";
+  }
+
+}
 function App() {
+
   return (
+
     <div className="App">
+
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
+        <img src={logo} className="App-logo" alt="logopipi" />
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ad Soyad: <input/> <br/>
+          E-Posta: <input/> @firat.edu.tr <br/>
+          Kullanıcı Adı: <input/> <br/>
+          Şifre: <input/> <br/>
+          Okuduğun Fakülte <input type={"search"}/> <br/>
+          Kaçıncı Sınıf: <input  type={"number"} min="1" max="7"/> <br/>
+          Mail bildirim almak istiyorum <input type={"checkbox"} id={"c1"} onClick={boxControl} /> <br/>
+
+          <p id="t1" style={{ display: "none" }}>
+          Bildirim alacağım mail adresi farklı olmasını istiyorum <input type={"checkbox"} id={"c2"} onClick={boxControl}/> <br/>
+          </p>
+
+          <p id="t2" style={{ display: "none" }}>
+          &nbsp;&nbsp;İkinci Mail adresi <input  type={"email"}/> <br/>
+          </p>
+          <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            KURALLARI
+          </a>
+          &nbsp;okudum ve kabul ediyorum <input type={"checkbox"} id={"c3"}/> <br/>
+          <input type={"button"} value={"Kayıt Ol"}/>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
+
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import RegisterP from "./registerP";
 
 test('renders learn react link', () => {
+
+  render(<RegisterP/>);
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
