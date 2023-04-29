92ed4216df9c7089bd530883ddd4b06507d9c212
aykq <asdesra@gmail.com>
2023-04-30 02:12:48 +0300

    bunlara devam edilecek.

--- src/profile.js
+++ src/profile.js
@@ -1,15 +1,14 @@
-import './profile.css'
+import './profile.css';
 
-function Profile(){
-    return(
-        <div className="Profile">
-            <header>
-
-            </header>
+function Profile() {
+    return (
+        <div>
+            <h3 id='profileInfoPanel'>profile info</h3>
+            <h1 id='profileTimeline'>profile timeline</h1>
+            <h1 id='posts'>posts</h1>
+            <h1 id='saved'>saved</h1>
+            <h1 id='comments'>comments</h1>
         </div>
-
-
-
     );
 }
 
