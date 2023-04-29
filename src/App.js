5a62be32a51ab4378594255a5b298c12c044c6ee
zeyd01 <zeydiyigoren@gmail.com>
2023-04-29 20:24:36 +0300

    registerSite3

--- src/App.js
+++ src/App.js
@@ -1,97 +1,43 @@
 import logo from './logo-01-01.png';
 import './App.css';
 
-function combinedClick1(){
-  openScript1();
-  valueControl();
-}
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import register from './register.js';
 
-function openScript1() {
-  window.open('registerP.js', '_blank');
-}
-function boxControl() {
-
-  const c1 = document.getElementById("c1");
-  const c2=document.getElementById("c2");
-  const t1 = document.getElementById("t1");
-  const t2=document.getElementById("t2");
-
-  if (c1.checked === true){
-
-      t1.style.display = "block";
-    } else {
-      t1.style.display = "none";
-    }
-  if (c2.checked && c1.checked === true){
-
-    t2.style.display = "block";
-  } else {
-    t2.style.display = "none";
-  }
-
-}
-function valueControl() {
-  const n1= document.getElementById("name").value.trim();
-  const m1= document.getElementById("mail1").value.trim();
-  const n2= document.getElementById("nickname").value.trim();
-  const p= document.getElementById("passW").value.trim();
-  const f= document.getElementById("fakulte").value.trim();
-  const c= document.getElementById("classNo").value.trim();
-  const r= document.getElementById("rules").value.trim();
-
-  if (n1 && m1 && n2 && p && f && c && r!=="") {
-    //Kayıt olma sitesinden başka yere yönlendirilecek
-  } else {
-    alert("Eksik değer girilmiş")
-  }
+function Register(){
+  return(
+      register() );
 }
 function App() {
-
   return (
 
-    <div className="App">
 
 
-      <header className="App-header">
-
-        <img src={logo} className="App-logo" alt="logopipi" />
-
-        <p>
-          <p id={"big"}>
-          Ad Soyad: <input id={"name"} defaultValue={null}/> <br/>
-          E-Posta: <input id={"mail1"}/> @firat.edu.tr <br/>
-          Kullanıcı Adı: <input id={"nickname"}/> <br/>
-          Şifre: <input id={"passW"} type={"password"}/> <br/>
-          Okuduğun Fakülte <input id={"fakulte"} type={"search"}/> <br/>
-          Kaçıncı Sınıf: <input id={"classNo"} type={"number"} min="1" max="7"/> <br/>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Kuralları
-          </a>
-          &nbsp;okudum ve kabul ediyorum <input type={"checkbox"} id={"c3"}/> <br/> <br/>
-
-            Mail bildirim almak istiyorum <input type={"checkbox"} id={"c1"} onClick={boxControl} /> <br/>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Welcome to development Home Page of <b>ZUBIDY</b>
           </p>
-          <p id="t1" style={{ display: "none" }}>
-            Farklı maile bildirim gönder <input type={"checkbox"} id={"c2"} onClick={boxControl}/> <br/>
-          </p>
+          <input id={"registerP"} type={"button"} value={"Register Page"} onClick={() => window.open('/register', '_blank')}/>
+        </header>
+      </div>
 
-          <p id="t2" style={{ display: "none" }}>
-            İkinci Mail adresi <input  type={"email"}/> <br/>
-          </p>
 
-          <input id={"rules"} type={"button"} value={"Kayıt Ol"} onClick={combinedClick1} />
-
-        </p>
-
-      </header>
-    </div>
   );
-
 }
 
-export default App;
+function Router() {
+  return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+  );
+}
+
+export default Router;
+
+
---
+++ src/register.js
@@ -0,0 +1,121 @@
+import $ from 'jquery';
+import './register.css';
+
+function register() {
+    $('input').focus(function(event) {
+        $(this).closest('.float-label-field').addClass('float').addClass('focus');
+
+    });
+    $('input').blur(function() {
+        $(this).closest('.float-label-field').removeClass('focus');
+        if (!$(this).val()) {
+            $(this).closest('.float-label-field').removeClass('float');
+        }
+    });
+    return (
+        <div className="register">
+            <header className="Register-header">
+
+                <fieldset className="field-header">
+                    <h3>Kayıt Ol</h3>
+
+                    <fieldset className="float-label-field">
+                        <label htmlFor="txtName">Ad Soyad</label>
+                        <input id="txtName" type="text" />
+                    </fieldset>
+
+                    <fieldset className="float-label-field">
+                        <label htmlFor="txtEmail">E-Posta</label>
+                        <input id="txtEmail" type="text" />
+                    </fieldset>
+
+                    <fieldset className="float-label-field">
+                        <label htmlFor="txtNickname">Kullanıcı Adı</label>
+                        <input id="txtNickname" type="text" minLength="4" maxLength="10" />
+                    </fieldset>
+
+                    <fieldset className="float-label-field">
+                        <label htmlFor="txtPassword">Şifre</label>
+                        <input id="txtPassword" type="password" />
+                    </fieldset>
+
+                    <fieldset className="float-label-field">
+                        <label htmlFor="txtConfirmPassword">Şifre Tekrarı</label>
+                        <input id="txtConfirmPassword" type="password" />
+                    </fieldset>
+
+                    <fieldset className="float-label-field">
+                        <label htmlFor="txtFakulte">Okuduğun Fakülte</label>
+                        <input id="txtFakulte" type="text" />
+                    </fieldset>
+
+                </fieldset>
+
+                <fieldset className="kuralMail-field">
+
+
+                    <a className="App-link"
+                       href="https://reactjs.org"
+                       target="_blank"
+                       rel="noopener noreferrer">
+                        Kuralları
+                    </a>
+                    &nbsp;okudum ve kabul ediyorum <input type={"checkbox"} id={"c3"}/> <br/> <br/>
+
+                    Mail bildirim almak istiyorum <input type={"checkbox"} id={"c1"} onClick={boxControl} /> <br/>
+
+                    <p id="t1" style={{ display: "none" }}>
+                        Farklı maile bildirim gönder <input type={"checkbox"} id={"c2"} onClick={boxControl}/> <br/>
+                    </p>
+
+                    <p id="t2" style={{ display: "none" }}>
+                        <fieldset className="float-label-field">
+                            <label htmlFor="txtIkiPosta">E-Posta</label>
+                            <input id="txtIkiPosta" type="email" />
+                        </fieldset>
+                    </p>
+
+                </fieldset>
+                <input id={"rules"} className={"rulesCss"} type={"button"} value={"Kayıt Ol"}  />
+            </header>
+        </div>
+    );
+}
+function boxControl() {
+
+    const c1 = document.getElementById("c1");
+    const c2=document.getElementById("c2");
+    const t1 = document.getElementById("t1");
+    const t2=document.getElementById("t2");
+
+    if (c1.checked === true){
+
+        t1.style.display = "block";
+    } else {
+        t1.style.display = "none";
+    }
+    if (c2.checked && c1.checked === true){
+
+        t2.style.display = "block";
+    } else {
+        t2.style.display = "none";
+    }
+
+}
+function valueControl() {
+    const n1= document.getElementById("txtName").value.trim();
+    const m1= document.getElementById("txtEmail").value.trim();
+    const n2= document.getElementById("txtNickname").value.trim();
+    const p= document.getElementById("txtPassword").value.trim();
+    const p2= document.getElementById("txtConfirmPassword".value.trim());
+    const f= document.getElementById("txtFakulte").value.trim();
+    const r= document.getElementById("rules").value.trim();
+
+    if (n1 && m1 && n2 && p && p2 && f && r!=="") {
+        //Kayıt olma sitesinden başka yere yönlendirilecek
+    } else {
+        alert("Eksik değer girilmiş")
+    }
+}
+
+export default register;
