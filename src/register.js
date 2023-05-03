d50fc7209ebd6185d814fead9e3c647d348a623a
zeyd01 <zeydiyigoren@gmail.com>
2023-05-03 14:18:14 +0300

    burak2

--- src/register.js
+++ src/register.js
@@ -1,17 +1,20 @@
 import $ from 'jquery';
 import './register.css';
-
-function register() {
+$(document).ready(function() {
     $('input').focus(function(event) {
         $(this).closest('.float-label-field').addClass('float').addClass('focus');
-
     });
+
     $('input').blur(function() {
         $(this).closest('.float-label-field').removeClass('focus');
         if (!$(this).val()) {
             $(this).closest('.float-label-field').removeClass('float');
         }
     });
+});
+function register() {
+
+
     return (
         <div className="register">
             <header className="Register-header">
